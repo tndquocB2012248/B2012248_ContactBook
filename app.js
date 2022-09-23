5690d5efe45ee207e61dfff6d7590404894cd6ea
tndquocB2012248 <quocb2012248@student.ctu.edu.vn>
2022-09-23 10:21:06 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -10,6 +10,8 @@ app.use(express.json());
 app.get("/", (req,res) => {
     res.json({ message: "Welcome to contact book application"}); 
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
 
